323c51520474a35048934c42fbfb2921d7be9401
James <jimiwhizz@gmail.com>
2019-02-06 21:34:59 +0000

    implemented user_voted_tag in ticket templates to check if user has voted on a ticket already and disable link if so

--- tickets/templatetags/app_tags.py
+++ tickets/templatetags/app_tags.py
@@ -1,7 +1,16 @@
 from django import template
-from .models import Vote
+from tickets.models import Vote
+from django.core.exceptions import ObjectDoesNotExist
 
 register = template.Library()
 
-@register.assignment_tag
-# def user_voted_tag():
+@register.simple_tag
+def user_voted_tag(ticket, user):
+    try:
+        vote = Vote.objects.get(ticket=ticket, username=user)
+    except ObjectDoesNotExist:
+        vote = None
+    if vote:
+        return True
+    else:
+        return False
