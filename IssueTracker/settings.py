b64ccf0acbf97e8b4f43da72831be17c6bde4008
James <jimiwhizz@gmail.com>
2019-02-19 18:54:32 +0000

    added import for whitenoise

--- IssueTracker/settings.py
+++ IssueTracker/settings.py
@@ -14,6 +14,7 @@ import os
 # from dotenv import load_dotenv
 import psycopg2
 import dj_database_url
+import whitenoise
 
 DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
